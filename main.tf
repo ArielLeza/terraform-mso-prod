282fbbe7ccabd65b16eb64f45b8286ec600bbc73
ArielLeza <ariel.leza@gmail.com>
2021-05-31 15:24:28 +0300

    main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+     = {
+      source = "/"
+      version = "0.1.5"
+    }
+  }
+}
+
+provider "mso" {
+    # cisco-mso user name
+    username = var.username
+    # cisco-mso password
+    password = var.password
+    # cisco-mso url
+    url      = "https://192.118.76.122:444/"
+    # domain   = ""
+    insecure = true
+}
