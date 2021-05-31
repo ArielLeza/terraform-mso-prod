a7c5d01867251fed1e2d51143b7bfe59e5ed6dc3
ArielLeza <ariel.leza@gmail.com>
2021-05-31 15:31:29 +0300

    remove vc

--- variables.tf
+++ variables.tf
@@ -6,8 +6,3 @@ variable "password" {
   type        = string
   description = "password"
 }
-variable "vc_password" {
-  sensitive   = true
-  type        = string
-  description = "Password of the account to be used with vCenter.  This should be the password for the account used to register vCenter with Intersight."
-}
